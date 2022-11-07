df54fa0d6e91dc215f19c05137c0bc14ea45f7b9
Vrushabh Veer <veervrushabh@gmail.com>
2022-11-07 16:38:53 +0530

    changes in db.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connection } = require("./config/db");
+const { connection } = require("./db");
 const { authentication } = require("./Middleware/authentication");
 const { todoRouter } = require("./Routes/Todo.route");
 const { userRouter } = require("./Routes/User.route");
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Todo");
+  res.send("Welcome to Todo");
 });
 
 app.use("/user", userRouter);
@@ -20,12 +20,12 @@ app.use(authentication);
 app.use("/todos", todoRouter);
 
 app.listen(port, () => {
-    try {
-        connection;
-        console.log("Conncted to DB successfully");
-    } catch (error) {
-        console.log("Error to connected to DB");
-        console.log(error);
-    }
-    console.log("Server is Running");
+  try {
+    connection;
+    console.log("Conncted to DB successfully");
+  } catch (error) {
+    console.log("Error to connected to DB");
+    console.log(error);
+  }
+  console.log("Server is Running");
 });
